44eaecbcf5be9578da35ba6e554c371dedbb9eb2
BlackFlame-hub <e.olteanu991@gmail.com>
2025-01-04 22:29:54 +0200

    Responsive/Mobile

---
+++ assets/_script/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerMenu = document.getElementById("burgermenu");
+  const burgerCross = document.getElementById("burgercross");
+  const navLinksBurger = document.querySelector(".nav-links_burger");
+
+  burgerCross.style.display = "none";
+
+  burgerMenu.addEventListener("click", () => {
+    navLinksBurger.style.display = "block";
+    burgerMenu.style.display = "none";
+    burgerCross.style.display = "block";
+  });
+
+  burgerCross.addEventListener("click", () => {
+    navLinksBurger.style.display = "none";
+    burgerMenu.style.display = "block";
+    burgerCross.style.display = "none";
+  });
+});
