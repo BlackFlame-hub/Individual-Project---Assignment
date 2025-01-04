f3a9beafb0f82351eb276ffbebad478923ef5c26
BlackFlame-hub <e.olteanu991@gmail.com>
2025-01-04 23:03:23 +0200

    Minor fix

--- assets/_script/script.js
+++ assets/_script/script.js
@@ -16,16 +16,4 @@ document.addEventListener("DOMContentLoaded", () => {
     burgerMenu.style.display = "block";
     burgerCross.style.display = "none";
   });
-
-  burgerMenu.addEventListener("touchstart", () => {
-    navLinksBurger.style.display = "block";
-    burgerMenu.style.display = "none";
-    burgerCross.style.display = "block";
-  });
-
-  burgerCross.addEventListener("touchstart", () => {
-    navLinksBurger.style.display = "none";
-    burgerMenu.style.display = "block";
-    burgerCross.style.display = "none";
-  });
 });
