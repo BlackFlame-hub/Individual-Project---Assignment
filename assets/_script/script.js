d48c71ab0b72763beb1c706254630374611945d1
BlackFlame-hub <e.olteanu991@gmail.com>
2025-01-20 23:30:32 +0200

    Fixed?

--- assets/_script/script.js
+++ assets/_script/script.js
@@ -4,9 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const navLinksBurger = document.querySelector(".nav-links_burger");
 
   burgerMenu.addEventListener("click", () => {
-    navLinksBurger.classList.add("active");
     navLinksBurger.classList.remove("hidden");
-
+    navLinksBurger.classList.add("active");
     burgerMenu.style.display = "none";
     burgerCross.style.display = "block";
   });
@@ -14,7 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
   burgerCross.addEventListener("click", () => {
     navLinksBurger.classList.remove("active");
     navLinksBurger.classList.add("hidden");
-
     burgerMenu.style.display = "block";
     burgerCross.style.display = "none";
   });
