e1ad80151997c3d83f5131dcc4ca8c1329a11df1
BlackFlame-hub <e.olteanu991@gmail.com>
2025-01-20 23:06:36 +0200

    Fixed?

--- assets/_script/script.js
+++ assets/_script/script.js
@@ -3,20 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const burgerCross = document.getElementById("burgercross");
   const navLinksBurger = document.querySelector(".nav-links_burger");
 
-  // Initial state
-  burgerCross.style.display = "none";
-
   burgerMenu.addEventListener("click", () => {
-    navLinksBurger.classList.add("active"); // Show the menu
-    navLinksBurger.classList.remove("hidden"); // Ensure it's not hidden
-    burgerMenu.style.display = "none"; // Hide the burger menu button
-    burgerCross.style.display = "block"; // Show the cross button
+    navLinksBurger.classList.add("active");
+    navLinksBurger.classList.remove("hidden");
+
+    burgerMenu.style.display = "none";
+    burgerCross.style.display = "block";
   });
 
   burgerCross.addEventListener("click", () => {
-    navLinksBurger.classList.remove("active"); // Hide the menu
-    navLinksBurger.classList.add("hidden"); // Ensure it's hidden
-    burgerMenu.style.display = "block"; // Show the burger menu button
-    burgerCross.style.display = "none"; // Hide the cross button
+    navLinksBurger.classList.remove("active");
+    navLinksBurger.classList.add("hidden");
+
+    burgerMenu.style.display = "block";
+    burgerCross.style.display = "none";
   });
 });
